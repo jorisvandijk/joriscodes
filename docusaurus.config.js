5b6a98f805741c78bdfd8400475f85c5ed7f5a01
Joris <joris.vandijk@student.fontys.nl>
2024-11-14 23:19:48 +0100

    Filling out image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: '',
+      image: 'img/docusaurus-social-card.png',
       navbar: {
         title: 'Joris.codes',
         logo: {
